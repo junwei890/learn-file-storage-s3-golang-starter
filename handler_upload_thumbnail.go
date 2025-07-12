d026f593079fd1c53853ebc414f51b99014d136e
jdub4asdfg <junweilee72@gmail.com>
2025-07-12 17:40:18 +0800

    updated handler that updates video thumbnails

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -31,7 +32,53 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	if err := r.ParseMultipartForm(maxMemory); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to parse request body", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to return file from form key", err)
+		return
+	}
+
+	mediaType := fileHeader.Header.Get("Content-Type")
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Media type not specified", err)
+		return
+	}
+
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read image data", err)
+		return
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video does not exist", err)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User ID mismatch", err)
+		return
+	}
+
+	videoThumbnail := thumbnail{
+		data: imageData,
+		mediaType: mediaType,
+	}
+	videoThumbnails[videoMetadata.ID] = videoThumbnail
+
+	newVideoThumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoIDString)
+	videoMetadata.ThumbnailURL = &newVideoThumbnailURL
+	if err := cfg.db.UpdateVideo(videoMetadata); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update video metadata", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
